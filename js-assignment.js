f089eaba378760ff2e18d7ac5dd984aa8ff0ac53
Olawale Sanmi-Afolabi <sanmiafolabi1759@gmail.com>
2026-07-26 14:13:58 +0100

    moved a copy of the Assignment from app.js to js-assignment.js

---
+++ js-assignment.js
@@ -0,0 +1,99 @@
+// Question 1
+
+const name = "Olawale Sanmi-Afolabi";
+const age = 90;
+const isEnrolled = true;
+console.log(name);
+console.log(age);
+console.log(isEnrolled);
+
+// Question 2
+let walletBalance = 5000;
+console.log(walletBalance);
+walletBalance = 7500;
+console.log(walletBalance);
+
+// Question 3
+const itemsInCart = 3;
+console.log(itemsInCart);
+// The value of itemsInCart is a whole number(Integer) and it is a constant value that cannot be changed.
+const itemPrice = 1.999;
+console.log(itemPrice);
+// The value of itemPrice is a decimal number(Float) and it is a constant value that cannot be changed.
+
+//Question 4
+const price = 2500
+const quantity = 4
+const total =price * quantity
+console.log(total);
+
+//Question 5
+const a = 17
+const b = 5
+const add = a + b
+const subtract = a - b
+const multiply = a * b
+const divide = a / b
+const modulus = a % b  
+const raiseToPower = a ** b 
+console.log(add)
+console.log(subtract)
+console.log(multiply)
+console.log(divide)
+console.log(modulus)
+console.log(raiseToPower)
+
+// Question 6
+const jerseyCost = 12000;
+const numberOfJerseys = 3;
+const deliveryFee = 2000;
+const totalCost = (jerseyCost * numberOfJerseys) + deliveryFee;
+console.log(totalCost);
+
+// Question 7
+const studentScore = 68;
+const passMark = 50;
+const isPassed = studentScore > passMark;
+const isHigherThan70 = studentScore >= 70;
+console.log(isPassed)
+console.log(isHigherThan70);
+
+// Question 8
+const priceA = 8000;
+const priceB = 12000;
+const isPriceAHigher = priceA > priceB;
+const isPriceALower = priceA < priceB;
+const isPriceALowerOrEqual = priceA <= priceB;
+const isPriceAHigherOrEqual = priceA >= priceB;
+console.log(isPriceAHigher)
+console.log(isPriceALower)
+console.log(isPriceALowerOrEqual)
+console.log(isPriceAHigherOrEqual)  
+
+//Question 9
+const num = 7;
+const str = "7";
+const isEqual = num == str;
+const isStrictlyEqual = num === str;
+console.log(isEqual)
+console.log(isStrictlyEqual);
+
+// Question 10
+const isInEqual = num != str;
+const isStrictlyInEqual = num !== str;
+console.log(isInEqual)
+console.log(isStrictlyInEqual);
+// what ! does is that it negates the value of the variable. For example, if the variable is true, it will become false and vice versa.
+
+//Question 11
+const isRaining = true;
+const isWeekend = false;
+console.log(!isRaining)
+console.log(!isWeekend);
+
+//Question 12
+const correctPin = 1234;
+const enteredPin = "1234";
+const isPinCorrect = correctPin === enteredPin;
+console.log(isPinCorrect);
+//
